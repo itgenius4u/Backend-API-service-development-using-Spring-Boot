7d9a49ec04d3455c9b711f3808f65f8e9859cf77
Dennis <itgenius1004@gmail.com>
2024-10-05 16:22:28 +0900

    ok

--- myapp2/src/main/java/com/example/myapp2/Myapp2Application.java
+++ myapp2/src/main/java/com/example/myapp2/Myapp2Application.java
@@ -1,13 +1,25 @@
 package com.example.myapp2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Myapp2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Myapp2Application.class, args);
+	}
+	@Autowired
+	private Student student;
+
+	@GetMapping("/name")
+	public String getStduentByName() {
+		student.setName("강감찬");		
+		return student.getName();
 	}
 
 }
