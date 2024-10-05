509becfaea86f9199877d77bf91fbcb2b376602a
Dennis <itgenius1004@gmail.com>
2024-10-05 16:18:04 +0900

    ok

--- myapp2/src/main/java/com/example/myapp2/Student.java
+++ myapp2/src/main/java/com/example/myapp2/Student.java
@@ -1,10 +1,20 @@
 package com.example.myapp2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Student {
     private String name;
+    private Address address;
+
+    public Address getAddress() {
+        return address;
+    }
+    @Autowired
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 
     public String getName() {
         return name;
