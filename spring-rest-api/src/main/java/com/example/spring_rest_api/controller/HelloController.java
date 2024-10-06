ac7c06204d8196a9813b1fd2f091803622b373cb
Dennis <itgenius1004@gmail.com>
2024-10-06 09:56:18 +0900

    ok

--- spring-rest-api/src/main/java/com/example/spring_rest_api/controller/HelloController.java
+++ spring-rest-api/src/main/java/com/example/spring_rest_api/controller/HelloController.java
@@ -1,12 +1,19 @@
 package com.example.spring_rest_api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.spring_rest_api.service.HelloService;
+
 @RestController
 public class HelloController {
+    @Autowired
+    private HelloService helloService;
     @GetMapping("/message")
     public String message() {
-        return "Spring Boot Hello";
+        helloService.setMessage("안녕하세요.");
+        return "Spring Boot Hello "
+            + helloService.getMessage();
     }
 }
---
+++ spring-rest-api/src/main/java/com/example/spring_rest_api/service/HelloService.java
@@ -0,0 +1,16 @@
+package com.example.spring_rest_api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
