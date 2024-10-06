686fee896afa6f7d5b361d230a6f636e971b9a5c
Dennis <itgenius1004@gmail.com>
2024-10-06 16:09:15 +0900

    ok

--- spring-rest-api/src/main/java/com/example/spring_rest_api/controller/StudentController.java
+++ spring-rest-api/src/main/java/com/example/spring_rest_api/controller/StudentController.java
@@ -5,16 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.spring_rest_api.entity.Student;
 import com.example.spring_rest_api.service.StudentService;
-
-import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -28,29 +28,32 @@ public class StudentController {
         return new ResponseEntity<>(students, HttpStatus.OK);
     }
     // GET /api/v1/students/{id}
+    @GetMapping("/students/{id}")
     public ResponseEntity<Student> getStudentById(@PathVariable("id") Integer id){
         Student student = studentService.getStudentById(id);
         return new ResponseEntity<>(student, HttpStatus.OK);
     }
 
     // POST /api/v1/students
+    @PostMapping("/students")
     public ResponseEntity<Student> createStudent(
         @RequestBody Student student){
         Student savedStudent = studentService.addStudent(student);
         return new ResponseEntity<>(savedStudent, HttpStatus.CREATED);
     }
     // DELETE /api/v1/students/{id}
+    @DeleteMapping("/students/{id}")
     public ResponseEntity<String> deleteStudent(
         @PathVariable Integer id) {
             studentService.deleteStudent(id);
             return new ResponseEntity<>("ok", HttpStatus.NO_CONTENT);
         }
     // PUT /api/v1/students/{id}
+    @PutMapping("/students/{id}")
     public ResponseEntity<Student> updateStudent(
         @PathVariable Integer id, @RequestBody Student student) {
            student.setStudentId(id);
            Student updatedStudent = studentService.updateStudent(student);
            return new ResponseEntity<>(updatedStudent, HttpStatus.NO_CONTENT);
-    
     }
 }
